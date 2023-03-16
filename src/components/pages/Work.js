ba666a4954be2a595de515bdeaa399c76fea89b3
Daniel Rodriguez <technoveltyco@users.noreply.github.com>
2023-03-16 23:47:22 +0000

    Add About and Skills

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -6,18 +6,8 @@ import projects from "../content/projects";
 
 function Work() {
   return (
-    <div>
-      <h1>Work Page </h1>
-      <h2>Previous</h2>
-      <Container>
-        <Row xs={1} md={2} className="g-4">
-          {projects.previous.map((project, id) => {
-            return <Project key={id} {...project}></Project>;
-          })}
-        </Row>
-      </Container>
-
-      <h2>Front End Boot Camp</h2>
+    <div className="Work">
+      <h1>Portfolio </h1>
       <Container>
         <Row xs={1} md={2} className="g-4">
           {projects.bootcamp.map((project, id) => {
